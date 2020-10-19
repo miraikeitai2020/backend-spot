60fef3d3e3b55f9381d6a1cafe3df759643436d6
SaKu2110 <b1018106@fun.ac.jp>
2020-10-20 02:51:31 +0900

    [Add] get spot handler

--- pkg/server/controller/controller.go
+++ pkg/server/controller/controller.go
@@ -1,9 +1,17 @@
 package controller
 
 import(
+	"net/http"
+
+	// import gin library
+	"github.com/gin-gonic/gin"
+
 	// Import SQL driver
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"github.com/miraikeitai2020/backend-spot/pkg/server/model"
+	"github.com/miraikeitai2020/backend-spot/pkg/utils"
 )
 
 type Controller struct {
@@ -15,3 +23,33 @@ func Init(db *gorm.DB) Controller {
 		DB: db,
 	}
 }
+
+func (ctrl *Controller) GetSpotsHandler(cxt *gin.Context) {
+	var request model.GetSpotsRequest
+	err := cxt.BindJSON(&request)
+	if err != nil {
+		utils.LogFatal(err)
+		cxt.JSON(
+			http.StatusInternalServerError,
+			utils.MakeErrorResponse(
+				http.StatusInternalServerError,
+				"Internal Server Error",
+				"Faild bind request JSON",
+			),
+		)
+	}
+
+	// TODO: Exec SQL query
+	spots := []model.Spot{}
+
+	cxt.JSON(
+		http.StatusOK,
+		utils.MakeGetSpotsResponse(spots),
+	)
+} 
+
+func (ctrl *Controller) GetDetoursHandler(cxt *gin.Context) {
+}
+
+func (ctrl *Controller) AddSpotHandler(cxt *gin.Context) {
+} 
