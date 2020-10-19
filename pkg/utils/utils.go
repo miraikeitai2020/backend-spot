de7a3cd02cfc18ba9c4bc1492c3f1a58b57f374a
SaKu2110 <b1018106@fun.ac.jp>
2020-10-20 02:52:40 +0900

    [Add] response struct generater

---
+++ pkg/utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import(
+	"log"
+	"github.com/miraikeitai2020/backend-spot/pkg/server/model"
+)
+
+func LogFatal(err error) {
+	log.Fatal(err)
+}
+
+func MakeErrorResponse(code int, msg, desc string) model.Error {
+	return model.Error{
+		Code: code,
+		Message: msg,
+		Description: desc,
+	}
+}
+
+func MakeGetSpotsResponse(spots []model.Spot) model.GetSpotsResponse {
+	return model.GetSpotsResponse{
+		Spots: spots,
+	}
+}
