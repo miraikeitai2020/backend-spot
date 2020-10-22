89f83ebe5e48180415b2896641068d5d668b2349
SaKu2110 <b1018106@fun.ac.jp>
2020-10-23 01:13:28 +0900

    [Delete] response generetor

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,41 +1,9 @@
 package utils
 
 import(
-	"log"
 	"time"
 	"math/rand"
-	"github.com/miraikeitai2020/backend-spot/pkg/server/model"
 )
-
-func LogFatal(err error) {
-	log.Fatal(err)
-}
-
-func MakeErrorResponse(code int, msg, desc string) model.Error {
-	return model.Error{
-		Code: code,
-		Message: msg,
-		Description: desc,
-	}
-}
-
-func MakeGetSpotResponse(spot model.Spot) model.GetSpotResponse {
-	return model.GetSpotResponse{
-		Spot: spot,
-	}
-}
-
-func MakeGetDetourResponse(detour []model.Detour) model.GetDetourResponse {
-	return model.GetDetourResponse{
-		Detour: detour,
-	}
-}
-
-func MakeMutationResponse(stat bool) model.MutationResponse {
-	return model.MutationResponse{
-		Status: stat,
-	}
-}
 
 func Random(min, max float64) float64 {
     rand.Seed(time.Now().UnixNano())
