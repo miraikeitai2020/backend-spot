9f0b229b2867171a8ea30c249bb54ceb98903bf4
SaKu2110 <b1018106@fun.ac.jp>
2020-10-23 02:26:13 +0900

    [Delete] QUERY_FORMAT_ADD_DETOURS

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -8,7 +8,6 @@ import(
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	
 	"github.com/miraikeitai2020/backend-spot/pkg/uuid"
-	"github.com/miraikeitai2020/backend-spot/pkg/utils"
 	"github.com/miraikeitai2020/backend-spot/pkg/config"
 	"github.com/miraikeitai2020/backend-spot/pkg/server/model"
 )
@@ -41,6 +40,5 @@ func ElectDetourInfo(db *gorm.DB) (info []model.Detour) {
 func InsertDetourInfoToDataBase(db *gorm.DB, r model.AddSpotRequest) {
 	id := uuid.GenerateUUID()
 	db.Exec(model.QUERY_FORMAT_ADD_DETOURS, id, r.Name, r.Description, r.Latitude, r.Longitude)
-	db.Exec(model.QUERY_FORMAT_ADD_DETOURS_EMOTION, id, utils.Random(0.0, 0.3), utils.Random(0.0, 0.3), utils.Random(0.0, 0.3), utils.Random(0.0, 0.3))
 	db.Exec(model.QUERY_FORMAT_ADD_DETOURS_IMAGE, id, []byte(r.Image))
 }
