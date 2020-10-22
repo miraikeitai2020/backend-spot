d0fdfa9bd6738a57676ec6538264cbfeb6f82629
SaKu2110 <b1018106@fun.ac.jp>
2020-10-23 02:08:47 +0900

    [Add] exec query function

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -29,15 +29,19 @@ func Init() (*gorm.DB, error) {
 	return nil, fmt.Errorf("Faild connection database")
 }
 
+func ElectSpotInfo(db *gorm.DB) (info []model.SpotInfo) {
+	db.Raw(model.QUERY_FORMAT_GET_SPOT).Scan(&info)
+	return
+}
+
+func ElectDetourInfo(db *gorm.DB) (info []model.Detour) {
+	db.Raw(model.QUERY_FORMAT_GET_DETOURS).Scan(&info)
+	return
+}
+
 func InsertDetourInfoToDataBase(db *gorm.DB, r model.AddSpotRequest) {
 	id := uuid.GenerateUUID()
 	db.Exec(model.QUERY_FORMAT_ADD_DETOURS, id, r.Name, r.Description, r.Latitude, r.Longitude)
 	db.Exec(model.QUERY_FORMAT_ADD_DETOURS_EMOTION, id, utils.Random(0.0, 0.3), utils.Random(0.0, 0.3), utils.Random(0.0, 0.3), utils.Random(0.0, 0.3))
 	db.Exec(model.QUERY_FORMAT_ADD_DETOURS_IMAGE, id, []byte(r.Image))
-}
-
-func ElectDetourInfo(db *gorm.DB, r model.GetDetourRequest) []model.Detour {
-	info := []model.Detour{}
-	db.Raw(model.QUERY_FORMAT_GET_DETOURS).Scan(&info)
-	return spot.DetourElection(r, info)
 }
