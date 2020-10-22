eb345aa3ca580a48a4a02bf9db0bbc30a49443d5
SaKu2110 <b1018106@fun.ac.jp>
2020-10-23 03:08:41 +0900

    [Update] spot filter

--- pkg/spot/spot.go
+++ pkg/spot/spot.go
@@ -6,13 +6,19 @@ import(
 	"github.com/miraikeitai2020/backend-spot/pkg/server/model"
 )
 
+// 地球の長半径
+const rx = 6378137.0
+
+type info struct {
+	Latitude	float64
+	Longitude	float64
+}
+
 func deg2rad(x float64) float64 {
 	return x * math.Pi / 180.0
 }
 
 func calcUserDistance(uLat, uLon, sLat, sLon float64) float64 {
-	rx := 6378137.0
-
 	x0 := deg2rad(uLat)
 	y0 := deg2rad(uLon)
 	x1 := deg2rad(sLat)
@@ -21,72 +27,73 @@ func calcUserDistance(uLat, uLon, sLat, sLon float64) float64 {
 	return rx * math.Acos(math.Sin(y0)*math.Sin(y1)+math.Cos(y0)*math.Cos(y1)*math.Cos(x0 - x1))
 }
 
-func isWithinRange(time int, lat, lon float64, s model.SpotInfo) bool {
-	return calcUserDistance(lat, lon, s.Latitude, s.Longitude) / 80.0 < float64(time)
+func isWithinRange(time float64, lat, lon float64, s info) bool {
+	return calcUserDistance(lat, lon, s.Latitude, s.Longitude) / 80.0 < time
 }
 
-func spotCandidates(time int, lat, lon float64, s []model.SpotInfo) (spots []model.SpotInfo) {
+func spotCandidates(time float64, lat, lon float64, s []model.SpotInfo) (spots []model.SpotInfo) {
 	for _, v := range s {
-		if isWithinRange(time, lat, lon, v) {
+		i := info{
+			Latitude: v.Latitude,
+			Longitude: v.Longitude,
+		}
+		if isWithinRange(time, lat, lon, i) {
 			spots = append(spots, v)
 		}
 	}
 	return
 }
 
-func isWithinRangeDetour(time float64, lat, lon float64, s model.Detour) bool {
-	return calcUserDistance(lat, lon, s.Latitude, s.Longitude) / 80.0 < time
-}
-
 func detourCandidates(time float64, lat, lon float64, s []model.Detour) (detour []model.Detour) {
 	for _, v := range s {
-		if isWithinRangeDetour(time, lat, lon, v) {
+		i := info{
+			Latitude: v.Latitude,
+			Longitude: v.Longitude,
+		}
+		if isWithinRange(time, lat, lon, i) {
 			detour = append(detour, v)
 		}
 	}
 	return
 } 
 
-func emotionFilter(emotion int, s []model.SpotInfo) model.Spot {
-	_s := model.SpotInfo{
-		Happiness: -1.0,
-		Natural: -1.0,
-		Sadness: -1.0,
-		Anger: -1.0,
-	}
-	// ここ改善よちアリ
-	// _s = _v の時のアルゴリズム
-	// P.S. 高速化できなかなぁ
-	for _, v := range s {
-		switch emotion {
-		case 1:
-			if _s.Happiness < v.Happiness {
-				_s = v
-			}
-		case 2:
-			if _s.Natural < v.Natural {
-				_s = v
-			}
-		case 3:
-			if _s.Sadness < v.Sadness {
-				_s = v
-			}
-		case 4:
-			if _s.Anger < v.Anger {
-				_s = v
-			}
+func comparisonValue(emotion int, _v, v model.SpotInfo) model.SpotInfo {
+	switch emotion {
+	case 1:
+		if _v.Happiness < v.Happiness {
+			_v = v
+		}
+	case 2:
+		if _v.Natural < v.Natural {
+			_v = v
+		}
+	case 3:
+		if _v.Sadness < v.Sadness {
+			_v = v
+		}
+	case 4:
+		if _v.Anger < v.Anger {
+			_v = v
 		}
 	}
+	return _v
+}
+
+func emotionFilter(emotion int, s []model.SpotInfo) model.Spot {
+	electionSpotInfo := s[0]
+	for _, v := range s {
+		electionSpotInfo = comparisonValue(emotion, electionSpotInfo, v)
+	}
 	return model.Spot{
-		ID: _s.ID,
-		Name: _s.Name,
-		Latitude: _s.Latitude,
-		Longitude: _s.Longitude,
+		ID: electionSpotInfo.ID,
+		Name: electionSpotInfo.Name,
+		Latitude: electionSpotInfo.Latitude,
+		Longitude: electionSpotInfo.Longitude,
 	}
 }
 
 func Election(r model.GetSpotRequest, s []model.SpotInfo) model.Spot {
-	spots := spotCandidates(r.Walktime, r.Latitude, r.Longitude, s)
+	spots := spotCandidates(float64(r.Walktime), r.Latitude, r.Longitude, s)
 	return emotionFilter(r.Emotion, spots)
 }
 
